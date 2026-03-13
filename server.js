787518ddc409cd7ff0b566bfc462ec85bf317c5f
Sironess <87657730+serhatovic73@users.noreply.github.com>
2026-03-13 21:12:51 +0100

    update

--- server.js
+++ server.js
@@ -3,29 +3,38 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 
 const app = express();
+
 app.use(cors());
+app.use(express.json());
 
-mongoose.connect(
-  "mongodb+srv://derdiedas_admin:BTlMtNhCeu1HzRuC@derdiedas.0svheys.mongodb.net/derdiedas",
-);
+// ⭐ MongoDB bağlantısı Render env variable'dan
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
 
+// ⭐ Model
 const Topic = mongoose.model(
   "topics",
   new mongoose.Schema({}, { strict: false }),
-  "topics", // collection adı
+  "topics",
 );
 
-// tüm konuları getir
+// ⭐ Endpoint
+app.get("/topic", async (req, res) => {
+  const topic = req.query.topic;
+  const data = await Topic.findOne({ topic });
+  res.json(data);
+});
+
 app.get("/topics", async (req, res) => {
   const data = await Topic.find();
   res.json(data);
 });
 
-// tek konu getir
-app.get("/topic", async (req, res) => {
-  const name = req.query.topic;
-  const data = await Topic.findOne({ topic: name });
-  res.json(data);
-});
+// ⭐ Render port ayarı
+const PORT = process.env.PORT || 5000;
 
-app.listen(5000, () => console.log("server çalıştı"));
+app.listen(PORT, () => {
+  console.log("Server running on " + PORT);
+});
